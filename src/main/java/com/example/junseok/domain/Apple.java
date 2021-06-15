236158792ace40feeeb16dcb41c691233ec6e2ba
shinjunseok <sjsage522@gmail.com>
2021-06-15 22:23:25 +0900

    update : apple domain
    필드(size: String) 추가

--- src/main/java/com/example/junseok/domain/Apple.java
+++ src/main/java/com/example/junseok/domain/Apple.java
@@ -4,6 +4,7 @@ public class Apple {
 
     private int weight;
     private String color;
+    private String size;  /* small or big */
 
     public Apple(int weight) {
         this.weight = weight;
@@ -12,6 +13,12 @@ public class Apple {
     public Apple(int weight, String color) {
         this.weight = weight;
         this.color = color;
+    }
+
+    public Apple(int weight, String color, String size) {
+        this.weight = weight;
+        this.color = color;
+        this.size = size;
     }
 
     public int getWeight() {
@@ -30,11 +37,20 @@ public class Apple {
         this.color = color;
     }
 
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
     @Override
     public String toString() {
         return "Apple{" +
                 "weight=" + weight +
                 ", color='" + color + '\'' +
+                ", size='" + size + '\'' +
                 '}';
     }
 }
